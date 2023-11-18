9f9410332f56d6bfa5a273e532c3688b0f1f038b
Ayan Dey <148696900+ayandey1359@users.noreply.github.com>
2023-11-18 17:22:30 +0530

    SQL-R_superstore DA

--- SQL/SQL_R_ExtensionScript.sql
+++ SQL/SQL_R_ExtensionScript.sql
@@ -3,6 +3,7 @@
    applying the constraint.*/
 -- first create a working database where all work will be done.
 -- the data base name is :: codeone
+
 CREATE DATABASE codeone;
 -- now show in the RDBMS SQL server how many database are there.
 SHOW DATABASES;
@@ -17,9 +18,20 @@ SELECT count(row_id)FROM customers   -- 9996 obs.
 SELECT count(row_id)FROM products    -- 9996 obs.
 SELECT count(row_id)FROM sales       -- 9996 obs.
 -- all number of obs. are same then itis ok to began mapping.
--- To make order_id as primary key in the superstore table
-ALTER TABLE superstore  ADD PRIMARY KEY (row_id) -- Error duplicate value
--- let fix it out 
+-- understand the data 
+SELECT * FROM superstore 
+DESC superstore 
+SELECT * FROM customers 
+DESC customers 
+SELECT * FROM products 
+DESC products 
+SELECT * FROM sales 
+DESC sales
+
+-- Understand and Clean the data one by one table 
+
+-- pre-cleaning observation 
+
 SELECT row_id, count( row_id) 
 FROM superstore 
 GROUP BY row_id HAVING count(row_id)> 1 
@@ -49,95 +61,157 @@ GROUP BY row_id HAVING count(row_id)> 1
 -- |------|--------------|
 -- |3     |2             |
 -- |9,821 |2             |
--- As I see all the duplicate are in the same obs. index
--- let see all those duplicate obs.
-SELECT * FROM superstore s LEFT JOIN customers c 
-       ON s.row_id=c.row_id
-LEFT JOIN products p 
-      ON s.row_id=p.row_id
-LEFT JOIN sales 
-      ON s.row_id=sales.row_id
-  WHERE s.row_id = 3 OR s.row_id= 9821;
--- let remove all those duplicates.
--- >> removing duplicate rows from superstore table
--- let see the duplicate obs.and frequency of superstore table 
+-- As I see all the duplicate are in the same obs. row_id
+
+-- work with customers table
+-- let see the duplicated values on customer table
+WITH customersCTE AS 
+     (SELECT *, ROW_NUMBER ()
+     OVER (PARTITION BY row_id ORDER BY row_id) AS index1
+     FROM customers)
+  SELECT * FROM customersCTE WHERE index1 > 1
+  -- there is two row duplicated 
+  -- let remove all the duplicated values 
+  /* here is one problem than the row_id also duplicated so there have no
+   * unique value which be my base to work with . thats why i am going to
+   * create a unique value by adding a primary key + auto increment with 
+   * a specific value and make it easy to clean. i will start from 1000 as
+   * a first value to ignore any union values of the partition variable.
+   */
+  -- add a temp column as primary key to clean the data
+  ALTER TABLE customers ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- make the index1 from 1000 to ignore union confusion 
+ ALTER TABLE customers
+ AUTO_INCREMENT = 1000;
+-- let see the data onece 
+SELECT * FROM customers
+-- now move to delete all the duplicate values
+DELETE FROM customers WHERE index1 IN (
+SELECT index1 FROM (SELECT index1,ROW_NUMBER() OVER (PARTITION BY row_id)AS frequency FROM customers)AS temp
+WHERE frequency > 1 )
+-- now all duplicated rows are deleted 
+-- now delete the temporary index and row_id
+ALTER TABLE customers DROP COLUMNS index1,row_id
+-- now do the same process with all tables 
+-- ==============================================================
+-- work with superstore table
+ -- let see the duplicated values on superstore table
 WITH superstoreCTE AS 
      (SELECT *, ROW_NUMBER ()
      OVER (PARTITION BY row_id ORDER BY row_id) AS index1
      FROM superstore)
   SELECT * FROM superstoreCTE WHERE index1 > 1
- -- delect all duplicate rows from superstore table.
-SELECT * FROM superstore WHERE row_id = 3
-|row_id|order_id      |order_date|ship_date|ship_mode   |
-|------|--------------|----------|---------|------------|
-|3     |CA-2016-138688|6/12/2016 |6/16/2016|Second Class|
-|3     |CA-2016-138688|6/12/2016 |6/16/2016|Second Class|
--- locate the duplicate value 
- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-|row_id|order_id      |order_date|ship_date|ship_mode     |
-|------|--------------|----------|---------|--------------|
-|3     |CA-2016-138688|6/12/2016 |6/16/2016|Second Class  |
-|3     |CA-2016-138688|6/12/2016 |6/16/2016|Second Class  |
-|9,821 |CA-2015-162201|6/8/2015  |6/12/2015|Standard Class|
-|9,821 |CA-2015-162201|6/8/2015  |6/12/2015|Standard Class|
--- let delet the duplicate rows
-/*-- DELETE  FROM superstore 
-WHERE row_id IN (SELECT row_id FROM (SELECT row_id ,count(row_id) AS frequency FROM superstore GROUP BY row_id
-HAVING frequency > 1) AS temp);
- SELECT row_id ,count(row_id) AS frequency FROM superstore GROUP BY row_id
- HAVING row_id= 2*/
- CREATE TABLE superstore_temp AS 
- SELECT DISTINCT * FROM superstore
- DROP TABLE superstore 
- ALTER TABLE superstore_temp 
- RENAME TO superstore
- -- Delete all duplicate row from customers table
- CREATE TABLE customers_temp AS 
- SELECT DISTINCT * FROM customers 
- DROP TABLE customers
- ALTER TABLE customers_temp 
- RENAME TO customers
- 
- WITH customersCTE AS 
-     (SELECT *, ROW_NUMBER ()
-     OVER (PARTITION BY row_id ORDER BY row_id) AS index1
-     FROM customers)
-  SELECT * FROM customersCTE WHERE index1 > 1
+  
+-- add a temp column as primary key to clean the data
+  ALTER TABLE superstore ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- make the index1 from 1000 to ignore union confusion 
+ ALTER TABLE superstore
+ AUTO_INCREMENT = 1000;
+-- let see the data onece 
+SELECT * FROM superstore
+-- now move to delete all the duplicate values
+DELETE FROM superstore WHERE index1 IN (
+SELECT index1 FROM (SELECT index1,ROW_NUMBER() OVER (PARTITION BY row_id)AS frequency FROM superstore)AS temp
+WHERE frequency > 1 )
+-- now all duplicated rows are deleted 
+-- now delete the temporary index and row_id
+delecte 
+-- ==========================================================
+-- let delete all the duplicate obs from products table
 
--- delect all duplicate rows from products table 
+-- let see the duplicated values on products table
 WITH productsCTE AS 
      (SELECT *, ROW_NUMBER ()
      OVER (PARTITION BY row_id ORDER BY row_id) AS index1
      FROM products)
   SELECT * FROM productsCTE WHERE index1 > 1
-CREATE TABLE products_temp AS 
- SELECT DISTINCT * FROM products
- DROP TABLE products
- ALTER TABLE products_temp 
- RENAME TO products
- -- Delete all the duplicate rows from sales table 
- WITH salesCTE AS 
+-- add a temp column as primary key to clean the data
+  ALTER TABLE products ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- make the index1 from 1000 to ignore union confusion 
+ ALTER TABLE products
+ AUTO_INCREMENT = 1000;
+-- let see the data onece 
+SELECT * FROM products
+-- now move to delete all the duplicate values
+DELETE FROM products WHERE index1 IN (
+SELECT index1 FROM (SELECT index1,ROW_NUMBER() OVER (PARTITION BY row_id)AS frequency FROM products)AS temp
+WHERE frequency > 1 )
+-- now all duplicated rows are deleted 
+-- now delete the temporary index and row_id
+ALTER TABLE products DROP COLUMNS index1,row_id
+-- ===========================================
+-- let delete all duplicated obs from sales table
+ -- let see the duplicated values on sales table
+WITH salesCTE AS 
      (SELECT *, ROW_NUMBER ()
      OVER (PARTITION BY row_id ORDER BY row_id) AS index1
      FROM sales)
   SELECT * FROM salesCTE WHERE index1 > 1
-  CREATE TABLE sales_temp AS 
- SELECT DISTINCT * FROM sales
- DROP TABLE sales
- ALTER TABLE sales_temp 
- RENAME TO sales
---  now all the duplicate rows are removed .
--- let connect all the table with constraint 
- -- let add primary key at the column of row_in on superstore table
-ALTER TABLE superstore ADD PRIMARY KEY(row_id)
-DESC superstore 
--- connect all table with superstore table with foreign mey 
-ALTER TABLE customers ADD FOREIGN KEY(row_id)REFERENCES superstore(row_id)
-DESC customers 
-ALTER TABLE sales ADD FOREIGN KEY(row_id)REFERENCES superstore(row_id)
-DESC sales 
-ALTER TABLE products ADD FOREIGN KEY(row_id)REFERENCES superstore(row_id)
--- now all the table are connected   
+-- add a temp column as primary key to clean the data
+  ALTER TABLE sales ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- make the index1 from 1000 to ignore union confusion 
+ ALTER TABLE sales
+ AUTO_INCREMENT = 1000;
+-- let see the data onece 
+SELECT * FROM sales
+-- now move to delete all the duplicate values
+DELETE FROM sales WHERE index1 IN (
+SELECT index1 FROM (SELECT index1,ROW_NUMBER() OVER (PARTITION BY row_id)AS frequency FROM sales)AS temp
+WHERE frequency > 1 )
+-- now all duplicated rows are deleted 
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/* now its time to connect all the table, i will delete all the tempory column which 
+ * i created and also delete those unwanted colum.  
+ */
+-- work with super store
+ALTER TABLE superstore DROP COLUMN index1 
+ALTER TABLE superstore DROP COLUMN row_id
+SELECT * FROM superstore
+-- let introduce another column which will work as key 
+ ALTER TABLE superstore ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- ===============================================================================
+-- work with products
+ALTER TABLE products DROP COLUMN index1 
+ALTER TABLE products DROP COLUMN row_id
+SELECT * FROM products
+-- let introduce another column which will work as key 
+ ALTER TABLE products ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- let connect products table with superstore table by foreign key
+ALTER TABLE products 
+ADD FOREIGN KEY (index1)REFERENCES superstore(index1)
+-- now superstore table and products table are connected
+-- ===============================================================================
+-- work with customers table
+ALTER TABLE customers DROP COLUMN index1 
+ALTER TABLE customers DROP COLUMN row_id
+SELECT * FROM customers
+-- let introduce another column which will work as key 
+ ALTER TABLE customers ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- let connect customers table with superstore table by foreign key
+ALTER TABLE customers 
+ADD FOREIGN KEY (index1)REFERENCES superstore(index1)
+-- now superstore table and customers table are connected
+-- =================================================================
+-- work with sales table
+ALTER TABLE sales DROP COLUMN index1 
+ALTER TABLE sales DROP COLUMN row_id
+SELECT * FROM sales
+-- let introduce another column which will work as key 
+ ALTER TABLE sales ADD index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
+-- let connect sales table with superstore table by foreign key
+ALTER TABLE sales 
+ADD FOREIGN KEY (index1)REFERENCES superstore(index1)
+-- now superstore table and sales table are connected
+
+-- confirmation of Mapping 
+SELECT count(*) FROM superstore -- 9994
+SELECT count(*) FROM products -- 9994
+SELECT count(*) FROM customers -- 9994
+SELECT count(*) FROM sales -- 9994
+-- now the data set is all connected and cleaned .. 
+
+-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2
+-- let jump in to analyse ! 
 
 -- As i need to see all the table frequently thats why I use stored Proecedure
 delimiter &&
@@ -147,7 +221,7 @@ delimiter &&
  END&&
 delimiter ;
   
-CALL superstore_tbl()
+CALL superstore_tbl() -- let see the RESULT BY this FUNCTION 
 
 delimiter &&
  CREATE PROCEDURE products_tbl()
@@ -186,9 +260,10 @@ WHERE row_id IN (SELECT row_id FROM sales WHERE sales >
 (SELECT avg(sales)FROM sales) 
 ORDER BY sales DESC )
 END // 
-delimiter ; -- > back TO defult delimiter
+delimiter ; -- > back TO defult delimiter(;)
  
 CALL top_ten_customer_sales(10)
+
 -- let see top three customers with maximum sales 
 CALL top_ten_customer_sales(3)
 
@@ -200,18 +275,17 @@ ON c.row_id = p.row_id
 INNER JOIN sales AS s
 ON c.row_id=s.row_id
 ORDER BY s.sales DESC 
+
 -- let display the virtual table 
-SELECT * FROM customer_sales_details 
+SELECT * FROM customer_sales_details
+
  -- long name make it shorter 
 RENAME TABLE customer_sales_details TO sales_details
+
 -- to list all the views name 
 SHOW FULL TABLES
 WHERE table_type='VIEW' 
 
-
- 
- 
- 
  -- row number
 WITH superstoreCTE AS 
      (SELECT *, ROW_NUMBER ()
@@ -220,19 +294,4 @@ WITH superstoreCTE AS
   SELECT * FROM superstoreCTE WHERE index1 > 1
 
  
- DELETE FROM demo 
- WHERE id IN (SELECT id from( SELECT id,ROW_NUMBER() OVER (PARTITION BY id ORDER BY id) AS index1 
- FROM demo ) AS temp WHERE index1 > 1)
  
-SELECT * FROM demo 
-
-ALTER TABLE demo 
-ADD COLUMN index2 int PRIMARY KEY FIRST 
-
-DROP COLUMN demo.index1
-ALTER TABLE demo 
-DROP COLUMN index1
-MODIFY COLUMN  index1 int PRIMARY KEY AUTO_INCREMENT=1000 
-	
-ALTER TABLE demo ADD COLUMN index1 int NOT NULL AUTO_INCREMENT  PRIMARY KEY FIRST;
-
